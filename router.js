9c8b3179669aac9cf489a8ba9bd45489f179f065
solmaz talebipour <soltalebipour@gmail.com>
2019-04-06 14:16:57 -0700

    updated router-post

--- router.js
+++ router.js
@@ -9,4 +9,71 @@ router.get('/profile', function(req, res, next) {
 });
 
 
+
+//POST route for updating data
+
+
+
+router.post('/', function (req, res, next) {
+
+    // confirm that user typed same password twice
+
+    if (req.body.password !== req.body.passwordConf) {
+
+        var err = new Error('Passwords do not match.');
+
+        err.status = 400;
+
+        return next(err);
+
+    }
+
+    if (req.body.email &&
+
+        req.body.username &&
+
+        req.body.password &&
+
+        req.body.passwordConf) {
+
+        var userData = {
+
+            email: req.body.email,
+
+            username: req.body.username,
+
+            password: req.body.password,
+
+            passwordConf: req.body.passwordConf,
+
+        }
+
+        //use schema.create to insert data into the db
+
+        User.create(userData, function (err, user) {
+
+            if (err) {
+
+                return next(err)
+            } else {
+                return res.redirect('/profile');
+            }
+        });
+
+    } else {
+        var err = new Error('All fields have to be filled out');
+        err.status = 400;
+        return next(err);
+    }
+});
+// POST route after registering
+
+router.post('/profile', function (req, res, next) {
+
+    return res.send('POST profile');
+
+});
+
 module.exports = router;
+
+
