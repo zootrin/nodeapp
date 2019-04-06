d6dae9c61a7642cc977faf6d500cee1a87632ff3
solmaz talebipour <soltalebipour@gmail.com>
2019-04-06 12:48:02 -0700

    updated app.js

--- app.js
+++ app.js
@@ -1,7 +1,63 @@
-console.log("ACIT2520 Project - Student Registration Webapp");
+var express = require('express');
+var app = express();
 
-console.log("Three Features to include --");
-console.log("1 -Authentication/Login");
-console.log("2 -Database Integration");
-console.log("3 -Administrator Access/Special Privileges");
 
+// serve static files from /public
+
+app.use(express.static(__dirname + '/template'));
+
+
+// include routes
+
+var routes = require('./routes/router');
+
+app.use('/', routes);
+
+
+// catch 404 and forward to error handler
+
+
+app.use(function(req, res, next) {
+
+
+    var err = new Error('File Not Found');
+
+    err.status = 404;
+
+    next(err);
+
+});
+
+
+// error handler
+
+// define as the last app.use callback
+
+app.use(function(err, req, res, next) {
+
+
+    res.status(err.status || 500);
+
+
+    res.json('error', {
+
+        message: err.message,
+
+        error: {}
+
+   });
+
+});
+
+
+// listen on port 8080
+
+
+
+app.listen(8080, function () {
+
+
+
+    console.log('Express app listening on port 8080');
+
+});
