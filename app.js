031acdbce908fc0f15408d3258807d8e93c6a003
Alasdair Rothnie <alasdair.rothnie@gmail.com>
2019-04-05 23:43:37 -0700

    2nd commit - github test

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log("Whaddup ACIT2520 Project")
+console.log("Whaddup ACIT2520 Project");
+console.log("Three Features to include --");
+console.log("1 -Authentication/Login");
+console.log("2 -Database Integration");
+console.log("3 -Administrator Access/Special Privileges");
