5da3b042ccb717b75a0235acf7dcac152cea45cc
solmaz talebipour <soltalebipour@gmail.com>
2019-04-06 15:29:54 -0700

    updated router_sol

--- router.js
+++ router.js
@@ -1,18 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var User = require('./models/user');
-
+var User = require('../models/user');
 
 // GET route for reading data
-router.get('/', function(req, res, next) {
-    return res.sendFile(__dirname + '/templateLogReg/index.html');
+
+router.get('/', function (req, res, next) {
+
+    return res.sendFile(path.join(__dirname + '/templateLogReg/index.html'));
+
 });
 
-
-
 //POST route for updating data
-
-
 
 router.post('/', function (req, res, next) {
 
@@ -23,6 +21,8 @@ router.post('/', function (req, res, next) {
         var err = new Error('Passwords do not match.');
 
         err.status = 400;
+
+        res.send("passwords dont match");
 
         return next(err);
 
@@ -44,36 +44,116 @@ router.post('/', function (req, res, next) {
 
             password: req.body.password,
 
-            passwordConf: req.body.passwordConf,
-
         }
 
-        //use schema.create to insert data into the db
+        User.create(userData, function (error, user) {
 
-        User.create(userData, function (err, user) {
+            if (error) {
 
-            if (err) {
+                return next(error);
 
-                return next(err)
             } else {
+
+                req.session.userId = user._id;
+
                 return res.redirect('/profile');
+
             }
+
+        });
+
+    } else if (req.body.logemail && req.body.logpassword) {
+
+        User.authenticate(req.body.logemail, req.body.logpassword, function (error, user) {
+
+            if (error || !user) {
+
+                var err = new Error('Wrong email or password.');
+
+                err.status = 401;
+
+                return next(err);
+
+            } else {
+
+                req.session.userId = user._id;
+
+                return res.redirect('/profile');
+
+            }
+
         });
 
     } else {
-        var err = new Error('All fields have to be filled out');
+
+        var err = new Error('All fields required.');
+
         err.status = 400;
+
         return next(err);
+
     }
+
+})
+
+// GET route after registering
+
+router.get('/profile', function (req, res, next) {
+
+    User.findById(req.session.userId)
+
+        .exec(function (error, user) {
+
+            if (error) {
+
+                return next(error);
+
+            } else {
+
+                if (user === null) {
+
+                    var err = new Error('Not authorized! Go back!');
+
+                    err.status = 400;
+
+                    return next(err);
+
+                } else {
+
+                    return res.send('<h1>Name: </h1>' + user.username + '<h2>Mail: </h2>' + user.email + '<br><a type="button" href="/logout">Logout</a>')
+
+                }
+
+            }
+
+        });
+
 });
-// POST route after registering
 
-router.post('/profile', function (req, res, next) {
+// GET for logout logout
 
-    return res.send('POST profile');
+router.get('/logout', function (req, res, next) {
+
+    if (req.session) {
+
+        // delete session object
+
+        req.session.destroy(function (err) {
+
+            if (err) {
+
+                return next(err);
+
+            } else {
+
+                return res.redirect('/');
+
+            }
+
+        });
+
+    }
 
 });
 
 module.exports = router;
-
-
